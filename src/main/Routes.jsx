b4803088848f642d7f7ceecf65cbddb885ffbb14
Joane Alves <joanealves2011@gmail.com>
2022-04-14 20:26:44 -0300

    Adjust Redirect routes

--- src/main/Routes.jsx
+++ src/main/Routes.jsx
@@ -3,15 +3,19 @@ import React from 'react';
 import Home from '../pages/home/Home';
 import Works from '../pages/works/Works';
 
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 const Routes = () => {
     return (
         <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/trabalhos" exact component={Works}>
-            </Route>          
-          </Switch>
+            <Route path="/trabalhos" exact component={Works} />
+
+            <Route path="/chuva_slaca" exact>
+                <Redirect to="/" />
+            </Route>
+
+        </Switch>
     );
 }
 
